49d25ed310935314084eb957152159913a22401d
Trevor Slocum <trevor@rocket9labs.com>
2025-07-11 11:30:44 -0700

    Workaround Go compiler bug affecting all Android versions before 12

--- game/mobile/mobile.go
+++ game/mobile/mobile.go
@@ -1,6 +1,8 @@
 package mobile
 
 import (
+	_ "unsafe" // Import unsafe to enable workaround below.
+
 	"codeberg.org/tslocum/boxcars/game"
 	"github.com/hajimehoshi/ebiten/v2/mobile"
 )
@@ -14,3 +16,9 @@ func init() {
 // gomobile will only compile packages that include at least one exported function.
 // Dummy forces gomobile to compile this package.
 func Dummy() {}
+
+// Workaround Go compiler bug affecting all Android versions before 12.
+// Issue: https://github.com/golang/go/issues/70508
+
+//go:linkname checkPidfdOnce os.checkPidfdOnce
+var checkPidfdOnce func() error
